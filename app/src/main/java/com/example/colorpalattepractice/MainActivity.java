aedd202413a808e0dec3ff816dc3f2f5e5c120f3
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2022-09-16 12:55:11 -0500

    9/16
    about to add button functionality

--- app/src/main/java/com/example/colorpalattepractice/MainActivity.java
+++ app/src/main/java/com/example/colorpalattepractice/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.colorpalattepractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button butoon;
+    TextView newText;
+    EditText newEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.newButton);
+        butoon = findViewById(R.id.newButoon);
+
+        newText = findViewById(R.id.helloText);
+        newEdit = findViewById(R.id.editTest);
+    }
+
+    public void shrinkTextMargin(){
+        
     }
 }
