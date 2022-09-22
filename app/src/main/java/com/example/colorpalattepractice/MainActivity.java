55093683984b54fc9db536a90926639943e3e19e
Sai Kovour <70875830+SuperSaigon123@users.noreply.github.com>
2022-09-22 00:40:11 -0500

    9/22 commit
    Project got messed up somehow while committing and the Main Activity got deleted. Had to restore previous files - couldn't finish full project tonight but done with code and will submit video tomorrow. Also had trouble running Android on computer for last 2 days.

--- app/src/main/java/com/example/colorpalattepractice/MainActivity.java
+++ app/src/main/java/com/example/colorpalattepractice/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.view.ViewGroup.MarginLayoutParams;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import java.lang.Object;
 
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button butoon;
     TextView newText;
     EditText newEdit;
+    TextView invisibleText;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
         newText = findViewById(R.id.helloText);
         newEdit = findViewById(R.id.editTest);
+        invisibleText = findViewById(R.id.newInvisibletext);
+
+        count = 0;
         
+    }
+
+    public void showSnackbar(View view){
+        invisibleText.setVisibility(View.INVISIBLE);
+        count++;
+        Snackbar.make(view, "counted: " + count, 2000).show();
+    }
+
+    public void showCount(View view){
+        invisibleText.setText(count + "");
+        invisibleText.setVisibility(View.VISIBLE);
     }
 }
